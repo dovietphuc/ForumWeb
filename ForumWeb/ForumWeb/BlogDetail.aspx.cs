cbb6929cb4319b655f0a0d99a4f6203ace24f313
Dray-OP <62054995+Dray-OP@users.noreply.github.com>
2021-11-20 17:19:06 +0700

    count view

--- ForumWeb/ForumWeb/BlogDetail.aspx.cs
+++ ForumWeb/ForumWeb/BlogDetail.aspx.cs
@@ -23,12 +23,23 @@ namespace ForumWeb
             user = (User)Session["user"];
             if(user !=null){
                 imgAvartar.ImageUrl = user.Avatar;
+                iViewCount();
             }
             if (IsPostBack) return;
             LoadDataRptrBlog();
             LoadDataBlogFile();
             LoadDataRptrUser();
             LoadDataRptrComment();
+        }
+        private void iViewCount()
+        {
+            var blogId = Request.QueryString["Id"];
+            SqlCommand cmd = new SqlCommand("update_view_blog", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@blogId", Convert.ToInt32(blogId));
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
         private void LoadDataRptrBlog()
         {
